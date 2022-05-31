1bbb81e175c8c6b67ad4b089bf19df80f180d924
mahdi.hasanpor <mahdi.hasanpour2022@gmail.com>
2022-05-31 01:53:50 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import { BrowserRouter } from "react-router-dom";
 // bootstrap 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
